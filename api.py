a87fcc8f4a478ddf3d20fba5356b3ad048cef4bd
msrosales <msrosales@up.edu.ph>
2024-09-24 16:09:08 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, request
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 #default route (not important)
 @app.route('/')
 def home():
